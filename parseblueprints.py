58b93176918d98bc40834365b0d23f71d7399b5b
Mohammad Akram <mohd.akram@tixilite.co.cc>
2012-07-06 10:19:21 +0400

    Removed temporary file

--- parseblueprints.py
+++
@@ -1,6 +0,0 @@
-from urllib2 import urlopen
-import re
-
-data = urlopen('http://www.tf2crafting.info/blueprints/').read()
-matches = re.findall(r'<td style="vertical\-align: top; padding-top: 18px;"><b>Fabricate ([^<]+)<\/b><\/td>(.+)',data,flags=re.DOTALL)
-print(matches)
